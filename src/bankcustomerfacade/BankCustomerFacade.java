230ecca70cd89fb12742fcc2788267eaef1bdd07
puwarinnueng <43135336+puwarinnueng@users.noreply.github.com>
2020-08-26 16:13:02 +0700

    edit

---
+++ src/bankcustomerfacade/BankCustomerFacade.java
@@ -0,0 +1,68 @@
+package bankcustomerfacade;
+
+import bankcustomers.BankAccount;
+import bankcustomers.BankCustomer;
+import gencustaccount.AccountIf;
+import gencustaccount.CustomerIf;
+import java.util.ArrayList;
+
+
+public class BankCustomerFacade {
+    private ArrayList<CustomerIf> customers = new ArrayList();
+    
+    public BankCustomerFacade() {
+        ArrayList<AccountIf> cust1Accounts = new ArrayList();
+        AccountIf acc = new BankAccount(111111);
+        acc.deposit(500);
+        cust1Accounts.add(acc);
+        acc = new BankAccount(222222);
+        acc.deposit(1000);
+        cust1Accounts.add(acc);
+        CustomerIf cust1 = new BankCustomer("Nueng", cust1Accounts);
+        customers.add(cust1);
+
+        ArrayList<AccountIf> cust2Accounts = new ArrayList();
+        acc = new BankAccount(333333);
+        acc.deposit(2000);
+        cust2Accounts.add(acc);
+        acc = new BankAccount(444444);
+        acc.deposit(3000);
+        cust2Accounts.add(acc);
+        CustomerIf cust2 = new BankCustomer("Strong", cust2Accounts);
+        customers.add(cust2);
+    }
+
+
+    public void doDeposit(double amt, CustomerIf cust, int accNo){
+        cust.getAccount(accNo).deposit(amt);
+    }
+
+    public AccountIf getBankAccount(CustomerIf cust, int accNo){
+        return cust.getAccount(accNo);
+    }
+
+    public CustomerIf getBankCustomer(String custName){
+        for(CustomerIf cus : customers){
+            if(cus.getCustomerName().equals(custName)){
+               return cus;
+            }
+        }
+
+        return null;
+    }
+
+    // additional
+
+    public AccountIf createAccount(int accNo,int amt){
+        AccountIf acc = new BankAccount(accNo);
+        acc.deposit(amt);
+        return acc;
+    }
+
+     public CustomerIf createCustomer(String name, ArrayList<AccountIf> accounts){
+         CustomerIf cus = new BankCustomer(name, accounts);
+         customers.add(cus);
+         return cus;
+     }
+
+}
--- src/bankfacade/BankFacade.java
+++ src/bankfacade/BankFacade.java
@@ -10,6 +10,7 @@ import bankcustomers.BankCustomer;
 import gencustaccount.AccountIf;
 import gencustaccount.CustomerIf;
 import java.util.ArrayList;
+import bankcustomerfacade.BankCustomerFacade;
 
 /**
  *
@@ -21,27 +22,34 @@ public class BankFacade {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       ArrayList<AccountIf> cust1Accounts = new ArrayList();
-       AccountIf acc = new BankAccount(12345);
-       acc.deposit(500);
-       cust1Accounts.add(acc);
-       acc = new BankAccount(12346);
-       acc.deposit(1000);
-       cust1Accounts.add(acc);
-       CustomerIf cust1 = new BankCustomer("John", cust1Accounts);
-       System.out.println("Customer and account information");
-       System.out.println("Name = " + cust1.getCustomerName());
-       System.out.println("Has " + cust1.getNumAccounts() + " accounts");
-       acc = cust1.getAccount(12345);
-       System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
-       acc = cust1.getAccount(12346);
-       System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
-       acc.deposit(1000);
-       System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
-       ArrayList<AccountIf> accounts = cust1.getllAccounts();
-       for(AccountIf account : accounts) {
-           System.out.println("Account number " + account.getAccountNumber() + " has " + account.getBalance());
-       }
+        BankCustomerFacade bankCustomerFacade = new BankCustomerFacade();
+        ArrayList<AccountIf> cust1Accounts = new ArrayList();
+        AccountIf acc;
+
+        acc = bankCustomerFacade.createAccount(12345,500);
+        cust1Accounts.add(acc);
+
+        acc = bankCustomerFacade.createAccount(12346,1000);
+        cust1Accounts.add(acc);
+
+        CustomerIf cust1 = bankCustomerFacade.createCustomer("Snow", cust1Accounts);
+        System.out.println("Customer and account information");
+        System.out.println("Name = " + cust1.getCustomerName());
+        System.out.println("Has " + cust1.getNumAccounts() + " accounts");
+
+        acc = bankCustomerFacade.getBankAccount(cust1, 12345);
+        System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
+
+        acc = bankCustomerFacade.getBankAccount(cust1, 12346);
+        System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
+
+        bankCustomerFacade.doDeposit(1000,cust1,12346);
+        System.out.println("Account Number: " + acc.getAccountNumber() + " has " + acc.getBalance());
+
+        ArrayList<AccountIf> accounts = bankCustomerFacade.getBankCustomer("Snow").getllAccounts();
+        for(AccountIf account : accounts) {
+            System.out.println("Account number " + account.getAccountNumber() + " has " + account.getBalance());
+        }
     }
     
 }
